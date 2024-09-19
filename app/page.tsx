3bad04c715e50080164aa3ab3940868af4f46ebb
Kazuki <sasa.9hoku@gmail.com>
2024-09-19 23:21:29 +0900

    added newarticle button

--- app/page.tsx
+++ app/page.tsx
@@ -24,28 +24,35 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-      {data.map((post, idx) => (
-        <Card key={idx}>
-          <Image
-            src={urlFor(post.titleImage).url()}
-            alt="image"
-            width={600}
-            height={700}
-            className="rounded-t-lg h-[200] object-cover"
-          />
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+        {data.map((post, idx) => (
+          <Card key={idx}>
+            <Image
+              src={urlFor(post.titleImage).url()}
+              alt="image"
+              width={600}
+              height={700}
+              className="rounded-t-lg h-[200] object-cover"
+            />
 
-          <CardContent className="mt-5">
-            <h3 className="text-lg font-bold line-clamp-2">{post.title}</h3>
-            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-              {post.smallDescription}
-            </p>
-            <Button asChild className="w-full mt-7">
-              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+            <CardContent className="mt-5">
+              <h3 className="text-lg font-bold line-clamp-2">{post.title}</h3>
+              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                {post.smallDescription}
+              </p>
+              <Button asChild className="w-full mt-7">
+                <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Button className="block w-full my-5 backgraound">
+        <Link href="https://next-blog14.sanity.studio/structure">
+          New Aticle
+        </Link>
+      </Button>
+    </>
   );
 }
